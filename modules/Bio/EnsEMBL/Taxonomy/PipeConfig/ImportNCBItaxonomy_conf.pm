c87fdcad082bac1494d62f08a7ea9ef28cc9b574
thomasmaurel <maurel@ebi.ac.uk>
2017-01-18 15:35:45 +0000

    Updated ressource classes for the EBI farm

--- modules/Bio/EnsEMBL/Taxonomy/PipeConfig/ImportNCBItaxonomy_conf.pm
+++ modules/Bio/EnsEMBL/Taxonomy/PipeConfig/ImportNCBItaxonomy_conf.pm
@@ -100,8 +100,8 @@ sub pipeline_create_commands {
 sub resource_classes {
     my ($self) = @_;
     return {
-         'default' => {'LSF' => '-q normal' },
-         'highmem' => {'LSF' => '-q normal -R"select[mem>20000] rusage[mem=20000]" -M20000' },
+         'default' => {'LSF' => '-q production-rh7' },
+         'highmem' => {'LSF' => '-q production-rh7 -M 20000 -R "rusage[mem=20000]"' },
     };
 }
 
